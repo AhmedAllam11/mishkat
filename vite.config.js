e21b7cbab2d4956d201a8d27120d033573a81bd2
Ahmed Allam <ahmadabusarie@gmail.com>
2026-04-23 00:10:23 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})git remote set-url origin https://github.com/AhmedAllam11/project.gitgit remote set-url origin https://github.com/AhmedAllam11/project.gitgit push -f origin main
+})
